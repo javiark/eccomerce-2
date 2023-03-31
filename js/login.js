52bdf729a8c2a77d4c40ea591772befebbe2cbf8
Javier <javier778@hotmail.com>
2023-03-31 15:41:28 -0300

    agregar Login

--- js/login.js
+++ js/login.js
@@ -6,35 +6,57 @@ loginForm.addEventListener("submit",(event)=>{
     event.preventDefault();
     console.dir(loginForm)
     // con la destructuracoin genero dos variables, email y password
+    //const el= loginForm.elements;
     const { email,password} = loginForm.elements;
 
     console.log(email.value, password.value)
     //2- Checkear datos ingresdos con los usuarios que tengo
     //2a-Obtener los usuarios almacenados
-    const users=JSON.parse(localStorage.getItem("user")) || [];
-    const user = user.find((usr)=>{
+    const users=JSON.parse(localStorage.getItem("users")) || []; // las comillas verticales son por si no encuentra nada, toma un array vacio y no va a ser null. Un array vacio es null
+    console.log(users)
+
+    const user = users.find((usr)=>{
         if (usr.email ===email.value){
             return true;        
         }
         return false;
     }) //objeto { name, password, email}
-    if(!user ){
-        alert("Los Datos ingresados no son correctos")
+
+    if(!user){ //sino existe usuario
+        alert("El usuario ingresado es incorrecto");
+        return;
     }
 
     if(user.password ===password.value){
         localStorage.setItem("currentUser", JSON.stringify(user));
+
+
         alert("Login correcto")
-    } else {
-        alert("Los Datos ingresados no son correctos")
-        return;
+    } 
+    else {
+        alert("El password es incorrecto")
+        return;}
+    });
+
+
+
+
+    //!------------OTRA FORMA----------------
+
+    // if(!user || user.password !== password.value ){ //sino existe usuario
+    //     alert("Los datos ingresados no son correctos");
+    //     return;
+    // }
+    //     localStorage.setItem("currentUser", JSON.stringify(user));
+    //     alert("Login correcto")
+
         
-    }
+
 
     
     // console.log(users)
-    // users.forEach(u=>console.log(u))
-});
+    // users.forEach(u=>console.log(u))}
+
 
 
 
