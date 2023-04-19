4ab434132fba1039e13adafd5963a709546b1ca8
Javier <javier778@hotmail.com>
2023-04-19 12:02:47 -0300

    footer

--- js/product-detail.js
+++ js/product-detail.js
@@ -25,43 +25,79 @@ const cardContainer=document.querySelector("#card-detail");
 
 function renderizarDetail(){
 cardContainer.innerHTML = ` 
-
-<main class="mainDetail">
-<section class="section-cards-detail">
-    <div class="cards-container-detail" id="card-container">
-  
-        <article class="card">
-            <div class="card__header">
-                <img src=${product.image} alt=${product.name} class="card__img">
-            </div>
-            <div class="card__body">
-                <div class="card__title">
-                <p>${product.name}</p>
-                </div>
-                <div class="card__description">
-                    <p class="card__texto">${product.detail}</p> 
-                    
-                </div>
-                <div class="card__info">
-                    <div class="card__date">
-                    12/01/2023
-                    </div>
-                    <div class="card__price">
-                    $ ${JSON.stringify(product.price)}
-                    </div>
-                </div>
-            </div>
-            <div class="card__footerCardDetail">
-                <a class="card__btn-buy" href="#">
-                    Comprar
-                </a>
-
-            </div>
-        </article> 
-        </main>
+<div class="mainDetail">
+    <div class="containerDetail">
+    <div class="containerDetail__imageContainer">
+        <img src=${product.image} alt=${product.name} class="containerDetail__image">
     </div>
-</section>
 
-`}
+    <div class="containerDetail__description">
+        <div class="containerDetail__container">
+            <div class="card__title">
+                <p>${product.name}</p>
+            </div>
+            <div class="card__price">
+            $ ${JSON.stringify(product.price)}
+            </div>
+            <div class="card__detail">
+                <p class="card__texto">${product.description}</p> 
+            </div>
+        </div>
+        <button class="containerDetail__btn-add">
+            Añadir a carrito
+        </button>
+        <button class="containerDetail__btn-buy">Comprar Ahora</button>
+    </div>
+    </div>
+
+    <div class="containerDetail">
+        <div class="containerDetail__descriptionLarge">
+            <h1 class="containerDetail__title">DESCRIPCION</h1>
+                <div class="containerDetail__detail">${product.detail}</div>
+        </div>
+    </div>
+</div> `
+
+}
 
 renderizarDetail()
+
+
+// cardContainer.innerHTML =`
+// {/* <main class="mainDetail">
+// <section class="section-cards-detail">
+//     <div class="cards-container-detail" id="card-container">
+  
+//         <article class="card">
+//             <div class="card__header">
+//                 <img src=${product.image} alt=${product.name} class="card__img">
+//             </div>
+//             <div class="card__body">
+//                 <div class="card__title">
+//                 <p>${product.name}</p>
+//                 </div>
+//                 <div class="card__description">
+//                     <p class="card__texto">${product.detail}</p> 
+                    
+//                 </div>
+//                 <div class="card__info">
+//                     <div class="card__date">
+//                     12/01/2023
+//                     </div>
+//                     <div class="card__price">
+//                     $ ${JSON.stringify(product.price)}
+//                     </div>
+//                 </div>
+//             </div>
+//             <div class="card__footerCardDetail">
+//                 <a class="card__btn-buy" href="#">
+//                     Comprar
+//                 </a>
+
+//             </div>
+//         </article> 
+//         </main>
+//     </div>
+// </section>
+
+// ` */}
