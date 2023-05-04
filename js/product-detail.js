3e0e936e2ce3a6a5e3877e76ef6c5c6018894998
Javier <javier778@hotmail.com>
2023-05-04 10:01:44 -0300

    arreglado descripcion detail

--- js/product-detail.js
+++ js/product-detail.js
@@ -39,34 +39,39 @@ cardContainer1.innerHTML = `
 <main class="main1">
     <div class="mainDetail">
         <div class="containerDetail">
-        <div class="containerDetail__imageContainer">
-            <img src=${product.image} alt=${product.name} class="containerDetail__image">
-        </div>
-
-        <div class="containerDetail__description">
-            <div class="containerDetail__container">
-                <div class="card__title">
-                    <p>${product.name}</p>
-                </div>
-                <div class="card__price">
-                $ ${JSON.stringify(product.price)}
-                </div>
-                <div class="card__detail">
-                    <p class="card__texto">${product.description}</p> 
-                </div>
+            <div class="containerDetail__imageContainer">
+                <img src=${product.image} alt=${product.name} class="containerDetail__image">
             </div>
-            <div class="containerDetail__containerAdd"><div class="containerDetail__containerBtn1"><button class="containerDetail__containerBtn" onclick=" restCount(indice) ">-</button><div class="containerDetail__containerNumber" id="countNumber"> 1</div><button  class="containerDetail__containerBtn" onclick=" addCount(indice) "> +</button></div></div>
 
-            <a href="/pages/order/order.html" class="containerDetail__btn-buy" >  <button class="containerDetail__btn-buy">Comprar Ahora</button><a/>
-        </div>
-        </div>
+                 <div class="containerDetail__description">
+                    <div class="containerDetail__container">
+                            <div class="card__title">
+                                <p>${product.name}</p>
+                            </div>
+                                    <div class="card__price">
+                                    $ ${JSON.stringify(product.price)}
+                                    </div>
+                                <div class="card__detail">
+                                    <p class="card__texto">${product.description}</p> 
+                    </div>
+                </div>
+                    <div class="containerDetail__containerAdd"><div class="containerDetail__containerBtn1"><button class="containerDetail__containerBtn" onclick=" restCount(indice) ">-</button><div class="containerDetail__containerNumber" id="countNumber"> 1</div><button  class="containerDetail__containerBtn" onclick=" addCount(indice) "> +</button></div></div>
 
-        <div class="containerDetail">
-            <div class="containerDetail__descriptionLarge">
-                <h1 class="containerDetail__title">DESCRIPCION</h1>
-                    <div class="containerDetail__detail">${product.detail}</div>
+                    <a href="/pages/order/order.html" class="containerDetail__btn-buy" >  <button class="containerDetail__btn-buy">Comprar Ahora</button><a/>
+                    </div>
+
+        </div>
+        <div class="containerDesc">
+
+            <div class="containerDesc__cont">
+              <h1 class="containerDesc__title">DESCRIPCION</h1>
+                    <div class="containerDesc__detail">${product.detail}</div>
             </div>
         </div>
+
+
+
+
     </div> 
 </main>`
 
